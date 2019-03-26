a0ae620ee641009eeb9ea22ad42c784883de7b3d
zm <2209078180@qq.com>
2019-03-26 21:19:21 +0800

    解决项目中删除的问题

--- microblog/js/add.js
+++ microblog/js/add.js
@@ -16,44 +16,42 @@ var sendBtn = document.getElementById('sendBtn') //广播
         }
     }
 
-    function getText(){     //获取输入的文本
+    function getText() {     //获取输入的文本
     return conBox.value;
     // alert(oMsg);
     }
-    function getUserName(){   //获取网名
+    function getUserName() {   //获取网名
         return userName.value;
     }
-    function getTime(){    //获取时间
+    function getTime() {    //获取时间
         let newTime = new Date();
-        return  ( `${newTime.getMonth()+1}月${newTime.getDate()}日
-                    ${newTime.getHours()}:${newTime.getMinutes()}`)
-            // newTime.getMonth()+1 + "月" + newTime.getDate() + "日" +"  "+ 
-            //      newTime.getHours() + ":" +newTime.getMinutes());
+        return  ( `${newTime.getMonth()+1}月 ${newTime.getDate()}日
+                    ${newTime.getHours()}: ${newTime.getMinutes()}`)
     }
 
     //获取头像 ---颜色
     var color = '';   //保存头像 ---颜色
-    for(let i =0 ;i<aImg.length;i++) {  
+    for(let i =0; i < aImg.length; i++) {  
         aImg[i].onclick = () => {color = aImg[i].value;}
     }
 
     function textLengthOverstep(length){  //文本长度超出
         countTxt.innerHTML = "已超出";
         maxNum.style.color = "red";
-        maxNum.innerHTML = length-140;
+        maxNum.innerHTML = length - 140;
     }
-    function textlengthWithIn(length){
+    function textlengthWithIn(length) {
         countTxt.innerHTML = "还可输入";
         maxNum.style.color = "#999";
         maxNum.innerHTML = 140-length;
     }
 
-    sendBtn.onclick = function(){  //广播
+    sendBtn.onclick = function() {  //广播
         // 判断
         if(!getUserName()){alert('请输入网名！'); return;}
         if(!color)
         {alert("请选择头像！"); return;}
-        if(!getText()){alert('请输入您想发表的内容！');return;}
+        if(!getText()) {alert('请输入您想发表的内容！');return;}
 
         // 满足发表内容的条件
         add();  //发表后将网名、头像、内容清空
@@ -63,33 +61,22 @@ var sendBtn = document.getElementById('sendBtn') //广播
 
     }
     var oUl = document.getElementsByTagName('ul')[0];
-    var oLi = oUl.children;
+    // var oLi = oUl.children;
 
-    for(let j = 0;j<oLi.length;j++) {
-        oLi[j].onmouseover = () => {   //鼠标移入
-            let del = document.getElementsByClassName('del')[j];
-            del.style.display = 'block';
-            //点击删除
-         }
-         oLi[j].onmouseout = () => {   //鼠标移出
-            let del = document.getElementsByClassName('del')[j];
-            del.style.display = 'none';
-         }
-    }
-    var del = document.getElementsByClassName('del');
+    // var del = document.getElementsByClassName('del');
 
-    for(let k = 0;k<del.length;k++) {
-        del[k].onclick = () => {
-            oUl.removeChild(oLi[k]);
-            return;
-        }
-    }
+    // for(let k = 0; k < del.length; k++) {
+    //     del[k].onclick = () => {
+    //         oUl.removeChild(oLi[k]);
+    //         return;
+    //     }
+    // }
 
     function add() {
         var newLi = document.createElement('li');
         let addLi = `<li><div class="userPic"><img src="" style="${color};"></div><div class="content">
         <div class="userName"><a href="javascript:;">${getUserName()}</a>:<div class="msgInfo">${getText()}</div>
-        </div><div class="times"><span>${getTime()}</span><a class="del" href="javascript:;" style="display:;">删除</a></div></div></li>`;
+        </div><a class="del" href="javascript:;" style="display:;"  onclick="deleteElement(this)" >删除</a><div class="times"><span>${getTime()}</span></div></div></li>`;
         newLi.innerHTML = addLi;
         oUl.insertAdjacentHTML('afterbegin',addLi);
     }
